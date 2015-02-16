a0d65086e8411971d252cdf9500d22a235ea3c80
Seth Chaiken <sdc@cs.albany.edu>
2015-02-16 10:29:30 -0500

    AMS 2005 presentation.

---
+++ Presentations/AMSoct05/abstract.pl
@@ -0,0 +1,98 @@
+<html>
+<head><title>Abstract Confirmation</title>
+<LINK REL="SHORTCUT ICON" HREF="/favicon.ico"> 
+<!-- begin style sheet link -->
+<link rel="stylesheet" type="text/css" href="/css_library/core_style.CSS">
+<!-- end style sheet link -->
+<script language="JavaScript">
+<!--
+function MM_reloadPage(init) {  //reloads the window if Nav4 resized
+  if (init==true) with (navigator) {if ((appName=="Netscape")&&(parseInt(appVersion)==4)) {
+    document.MM_pgW=innerWidth; document.MM_pgH=innerHeight; onresize=MM_reloadPage; }}
+  else if (innerWidth!=document.MM_pgW || innerHeight!=document.MM_pgH) location.reload();
+}
+MM_reloadPage(true);
+// -->
+</script>
+</head>
+
+<BODY>
+<table width="100%" border="0">
+  <tr>
+    <td width="14%"><a href="/"> 
+    <img align="middle" alt="AMS WEBSITE" border="0" hspace="5" 
+    src="/images/ams-logo.gif" width="120" height="88"></A>
+    </td>
+    <td width="86%">
+    <h2>Abstract Confirmation</h2>
+    </td>
+  </tr>
+</table>
+<HR>
+
+
+
+<xml id="mtg_intro_begin"></xml>
+<P>
+Your abstract has been successfully processed for the
+<b>
+Annandale-on-Hudson, New York
+</b> meeting.
+</P>
+<P>
+<b>Your abstract number is:</b> 
+1009-05-125.
+<br>
+You must refer to this number in any correspondence concerning this abstract.
+Once your abstract is approved, you will receive an
+e-mail message regarding the date, time, and location of your
+presentation approximately two weeks after the abstract deadline, or
+four weeks for annual meetings.
+</P>
+<P>
+If your abstract has been submitted for the incorrect meeting or incorrect 
+session, please send email to
+<a href="mailto:abs-info@ams.org">abs-info@ams.org</a>.
+</P>
+<xml id="mtg_intro_end"></xml>
+
+<!-- BEGIN AMS copyright FOOTER. DO NOT ALTER THIS SECTION. 
+     DO NOT REMOVE THIS SECTION FROM EDITED VERSIONS OF THIS FILE.   -->
+<table border="0" cellpadding="2" cellspacing="0" width="100%"
+summary="This is a footer table containing contact information, 
+privacy and copyright statements">
+<tr>
+<td colspan=4>
+<hr noshade size="1" color="#800000">
+</td>
+</tr>
+<tr>
+    <td width="30%" align="right"></td> 
+    <td width="40%" align="center"> 
+      <!-- Begin AMS Website Logo -->
+      <a href="/"><img border="0" src="/images/ams-logo-sm.gif" alt="AMS Website Logo Small" width="40" height="30"></a> 
+      <!-- End AMS Website Logo -->
+      <font size="2">  Comments: <a href="mailto:webmaster@ams.org" CLASS="footer">webmaster@ams.org</a><br>
+      <!-- Begin AMS copyright statement -->
+      <a href="/ams/copyright.html" CLASS="footer">&copy; Copyright 2005, American
+      Mathematical
+Society
+      <!-- End AMS copyright statement -->
+      <br>
+      <!-- Begin AMS privacy statement -->
+      </a> <a href="/ams/privacy.html" CLASS="footer">Privacy Statement</a></font> 
+    <!-- End AMS privacy statement --></td>
+    <td width="15%" align="right"></td> 
+<td width="15%" align="center"><a href="http://www.ams.org/sitesearch" CLASS="footer"><strong>Search the AMS</strong></a>&nbsp;&nbsp;&nbsp;&nbsp;
+  <a href="http://www.ams.org/sitesearch" CLASS="footer"><img src="/images/powered_by_google.gif" alt="Powered by Google" width="135" height="35" border="0" align="left"></a></td>
+</tr>
+</table>
+
+<!-- END AMS copyright FOOTER. DO NOT ALTER THIS SECTION. 
+     DO NOT REMOVE THIS SECTION FROM EDITED VERSIONS OF THIS FILE.  -->
+
+
+
+
+</body>
+</html>
